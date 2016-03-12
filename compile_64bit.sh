bd3938e1cebec3807168abdbfb2ccc0ec2782ccd
Puru Pathak <puruppathak@gmail.com>
2016-03-12 16:47:29 -0500

    Added files via upload

---
+++ compile_64bit.sh
@@ -0,0 +1,8 @@
+g++ -o ghotekar-pathak-encrypt ghotekar-pathak-encrypt.cpp -lssl -lcrypto
+g++ -o ghotekar-pathak-authorize ghotekar-pathak-authorize.cpp -lssl -lcrypto
+g++ -o ghotekar-pathak-decrypt ghotekar-pathak-decrypt.cpp -lssl -lcrypto
+g++ -g -O -c ghotekar-pathak-aws-s3-cloud-upload.cpp -Ichilkat-9.5.0-x86_64-linux-gcc/include/
+g++ -o ghotekar-pathak-aws-s3-cloud-upload ghotekar-pathak-aws-s3-cloud-upload.o -L./chilkat-9.5.0-x86_64-linux-gcc/lib -lchilkat-9.5.0 -lresolv -lpthread
+g++ -g -O -c ghotekar-pathak-aws-s3-cloud-download.cpp -I chilkat-9.5.0-x86_64-linux-gcc/include/
+g++ -o ghotekar-pathak-aws-s3-cloud-download ghotekar-pathak-aws-s3-cloud-download.o -L./chilkat-9.5.0-x86_64-linux-gcc/lib -lchilkat-9.5.0 -lresolv -lpthread
+
